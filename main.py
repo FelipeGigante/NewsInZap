52fe5e5ae47b410c2a9d6baaf8a493ee46072c0b
Felipe Gigante <lipe.gigante2004@gmail.com>
2024-05-20 15:18:28 -0300

    alterando libs e reformulando código

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import time
+import requests
+import bs4
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+url = 'https://ge.globo.com/'
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://flipboard.com/@flipboardbr/edi-o-do-dia-vonp8btnz")
+request = requests.get(url)
 
-
-news_body_list = []
-news_title_list = []
+pagehtml = bs4.BeautifulSoup(request.text, "html.parser")
+list_news = pagehtml.find_all("a", class_="feed-post-link")
 
 
-for i in range(1, 5):
-    # Encontre os elementos de título e corpo da notícia
-    news_title = driver.find_element(By.XPATH, "//*[@id='content']/div/div/div/div[2]/main/ul/li[{}]/div/article/div/h3/a".format(i))
-    news_body = driver.find_element(By.XPATH, "//*[@id='content']/div/div/div/div[2]/main/ul/li[{}]/div/article/div/h3/a".format(i))
-    
-    # Adicione o texto dos elementos às listas
-    news_title_list.append(news_title)
-    news_body_list.append(news_body)    
-    
-    # Imprima o título e o corpo da notícia
-    print(news_title)
-    print(news_body)
-
-# Imprima as listas de títulos e corpos das notícias
-print(news_title_list)
-print(news_body_list)
-
-
-#news_list = driver.find_elements(By.XPATH, "//*[@id='content']/div/div/div/div[2]/main/ul")
-#news_data = []
-
-# Percorrer os itens da lista <ul>
-#for news_item in news_list:
-#    news_data.append(news_item.text)
-
-# Imprimir os dados das notícias
-#for news in news_data:
-#    print(news)
-
-
-driver.quit()
+for news in list_news:
+    print(news.text)
+    print(news.get("href"))
